2a9cfde60ffe0850959632edf20f6e001c4e8856
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 15:23:36 -0300

    feat: implements basic login Page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import Button from '../Components/Button';
+import Input from '../Components/Input';
 
 function Login() {
   return (
-    <p>hi</p>
+    <div>
+      <Input id="email" type="email" name="email" value="" label="Email" />
+      <Input id="password" type="password" name="password" value="" label="password" />
+      <Button text="Entrar" />
+    </div>
   );
 }
 
