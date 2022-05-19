ab34444942603d5e868cd83f9df86029c55333b9
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 16:51:40 -0300

    feat: add task route

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './index.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Login from './Pages/Login';
+import Tasks from './Pages/Tasks';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/tasks" element={<Tasks />} />
     </Routes>
   );
 }
