d469f3a76dae7190d4b679479a50b88e9e730229
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 16:51:08 -0300

    feat: implement function to handle click event

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,14 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../Styles/Components/Button.css';
 
-function Button({ text }) {
+function Button({ text, onClick, disabled }) {
   return (
-    <button className="button" type="button">{text}</button>
+    <button className="button" type="button" disabled={disabled} onClick={onClick}>{text}</button>
   );
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  disabled: true,
 };
 
 export default Button;
