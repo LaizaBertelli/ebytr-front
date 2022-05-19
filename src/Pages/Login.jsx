ecb8672313affa81db1a18c789ee6d4ed4d7eefb
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 15:56:57 -0300

    feat: implements basic login styles

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import Button from '../Components/Button';
 import Input from '../Components/Input';
+import '../Styles/Pages/Login.css';
 
 function Login() {
   return (
-    <div>
-      <Input id="email" type="email" name="email" value="" label="Email" />
-      <Input id="password" type="password" name="password" value="" label="password" />
-      <Button text="Entrar" />
+    <div className="login_main">
+      <div className="login_forms">
+        <h1>Login</h1>
+        <Input id="email" type="email" name="email" value="" label="Email" />
+        <Input id="password" type="password" name="password" value="" label="Password" />
+        <Button text="Entrar" />
+      </div>
+      <p>Não tem uma conta? Cadastre-se aqui</p>
     </div>
   );
 }
