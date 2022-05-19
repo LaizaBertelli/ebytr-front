71611cb3c9c70a466bc88d040c129e400d21aff0
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 15:56:30 -0300

    feat: implements basic button styles

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Styles/Components/Button.css';
 
 function Button({ text }) {
   return (
-    <button type="button">{text}</button>
+    <button className="button" type="button">{text}</button>
   );
 }
 
