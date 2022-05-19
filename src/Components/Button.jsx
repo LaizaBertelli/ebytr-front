365d030efdf19db29eba50b94733cfc028117f33
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 20:38:46 -0300

    fix: edit button component to me more flexible

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,14 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../Styles/Components/Button.css';
 
-function Button({ text, onClick, disabled }) {
+function Button({
+  text, onClick, disabled, customClass,
+}) {
   return (
-    <button className="button" type="button" disabled={disabled} onClick={onClick}>{text}</button>
+    <button className={customClass} type="button" disabled={disabled} onClick={onClick}>{text}</button>
   );
 }
 
 Button.propTypes = {
   text: PropTypes.string.isRequired,
+  customClass: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
   disabled: PropTypes.bool,
 };
