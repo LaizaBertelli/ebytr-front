1072938496ad6b80ee4fd0552dfcaeb4e090cff0
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 19:01:18 -0300

    feat: implements header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Styles/Components/Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h2>Task Manager</h2>
+    </header>
+  );
+}
+
+export default Header;
