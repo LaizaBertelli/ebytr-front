426ef69680309e12943a3619b61bab5aaec6f544
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 20:40:08 -0300

    feat: implements function to request tasks

--- src/Services/requests.js
+++ src/Services/requests.js
@@ -1,18 +1,23 @@
 import axios from 'axios';
 
-const requestLogin = async (email, password) => {
-  const token = axios({
+export const requestLogin = async (email, password) => {
+  const { data } = await axios({
     method: 'post',
     url: 'http://localhost:3080/login/',
     data: {
       email,
       password,
     },
-  })
-    .then((response) => response.data)
-    .catch((error) => error.code);
+  });
 
-  return token;
+  return data;
 };
 
-export default requestLogin;
+export const requestTasks = async (id) => {
+  const { data } = await axios({
+    method: 'get',
+    url: `http://localhost:3080/tasks/${id}`,
+  });
+
+  return data;
+};
