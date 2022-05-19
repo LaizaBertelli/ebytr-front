847a426891c198ac2a4add668adb11ba859d6539
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 15:56:43 -0300

    feat: implements basic input styles

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../Styles/Components/Input.css';
 
 function Input({
   id, type, name, value, label,
 }) {
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
-      <input type={type} id={id} name={name} value={value} />
+      <label className="label" htmlFor={id}>{label}</label>
+      <input className="input" type={type} id={id} name={name} value={value} />
     </div>
   );
 }
