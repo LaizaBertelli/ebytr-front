16e89319d75a1851d5b334f79318d26e676a1e8f
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 20:38:15 -0300

    feat: implements tasks cards component

---
+++ src/Components/TaskCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from './Button';
+import '../Styles/Components/TaskCard.css';
+
+function TaskCard({ title, description, status }) {
+  const handleEdit = () => {
+    console.log('clicked edit');
+  };
+
+  const handleDelete = () => {
+    console.log('clicked delete');
+  };
+  return (
+    <div className="task_card">
+      <h3>{ title }</h3>
+      <p>{ description }</p>
+      <p>{ status }</p>
+      <section className="task_card_buttons">
+        <Button customClass="edit_button" text="Editar" disabled={false} onClick={handleEdit} />
+        <Button customClass="delete_button" text="Deletar" disabled={false} onClick={handleDelete} />
+      </section>
+    </div>
+  );
+}
+
+TaskCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+};
+
+export default TaskCard;
