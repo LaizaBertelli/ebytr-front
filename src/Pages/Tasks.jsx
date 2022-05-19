4825e1ce02888e3ce5f3ed9e051b255a548ba737
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 20:39:22 -0300

    feat: fetch and render user's tasks

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -1,17 +1,46 @@
-import React, { useEffect } from 'react';
+/* eslint-disable no-unused-expressions */
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Components/Header';
+import TaskCard from '../Components/TaskCard';
+import { requestTasks } from '../Services/requests';
+import '../Styles/Pages/Task.css';
 
 function Tasks() {
   const navigate = useNavigate();
+  const [tasks, setTasks] = useState([]);
+
+  // redirect to login page if not logged in
   useEffect(() => {
     const isLogged = JSON.parse(localStorage.getItem('logged'));
     if (!isLogged) navigate('/');
-    console.log(isLogged);
   });
+
+  useEffect(() => {
+    const { id } = JSON.parse(localStorage.getItem('user'));
+    const getTasks = async (userId) => {
+      const data = await requestTasks(userId);
+      setTasks(data.tasks);
+    };
+
+    getTasks(id)
+      .catch(console.error);
+  }, []);
+
   return (
     <div>
       <Header />
+      <div className="cards_container">
+        {
+          tasks.map((task) => (
+            <TaskCard
+              title={task.title}
+              description={task.description}
+              status={task.status}
+            />
+          ))
+        }
+      </div>
     </div>
   );
 }
