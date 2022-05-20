464975dbe9e043088e4f21310e897e66c7813f39
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-20 15:57:52 -0300

    feat: change request url to live api

--- src/Services/requests.js
+++ src/Services/requests.js
@@ -3,28 +3,42 @@ import axios from 'axios';
 export const requestLogin = async (email, password) => {
   const { data } = await axios({
     method: 'post',
-    url: 'http://localhost:3080/login/',
+    url: 'https://lb-task-manager.herokuapp.com/login',
     data: {
       email,
       password,
     },
   });
-
   return data;
 };
 
 export const requestTasks = async (id) => {
   const { data } = await axios({
     method: 'get',
-    url: `http://localhost:3080/tasks/${id}`,
+    url: `https://lb-task-manager.herokuapp.com/tasks/${id}`,
   });
 
+  return data;
+};
+
+export const requestCreateTask = async (userId, status, description, title) => {
+  const { data } = await axios({
+    method: 'post',
+    url: `https://lb-task-manager.herokuapp.com/tasks/${userId}`,
+    data: {
+      userId,
+      status,
+      description,
+      title,
+    },
+  });
+  console.log(data);
   return data;
 };
 
 export const requestDeleteTask = async (id) => {
   await axios({
     method: 'delete',
-    url: `http://localhost:3080/tasks/${id}`,
+    url: `https://lb-task-manager.herokuapp.com/tasks/${id}`,
   });
 };
