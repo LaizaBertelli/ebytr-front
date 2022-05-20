237e94cc11451c1ff464c0b9f8e303226577a929
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 23:24:39 -0300

    fix: fix logic to delete task using context

--- src/Components/TaskCard.jsx
+++ src/Components/TaskCard.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import Button from './Button';
-import NewTask from './NewTask';
+import { GrAddCircle } from 'react-icons/gr';
 import '../Styles/Components/TaskCard.css';
 import '../Styles/Components/Button.css';
+import Button from './Button';
+import { requestDeleteTask } from '../Services/requests';
+import TasksContext from '../Context/TasksContext';
 
-function TaskCard({ title, description, status }) {
-  const handleEdit = () => {
-    console.log('clicked edit');
-  };
-
-  const handleDelete = () => {
-    console.log('clicked delete');
+function TaskCard({
+  title, description, status, id,
+}) {
+  const { tasks, setTasks } = useContext(TasksContext);
+  const handleDelete = async () => {
+    await requestDeleteTask(id);
+    const newTasks = tasks.filter((task) => task.id !== id);
+    setTasks([...newTasks]);
   };
 
   if (status === 'new') {
-    return (<NewTask />);
+    return (
+      <div className="task_card new_task">
+        <GrAddCircle size={70} />
+      </div>
+    );
   }
 
   return (
@@ -23,9 +30,8 @@ function TaskCard({ title, description, status }) {
       <h3>{ title }</h3>
       <p>{ description }</p>
       <p>{ status }</p>
-      <section className="task_card_buttons">
-        <Button customClass="edit_button" text="Editar" disabled={false} onClick={handleEdit} />
-        <Button customClass="delete_button" text="Deletar" disabled={false} onClick={handleDelete} />
+      <section>
+        <Button text="Deletar" disabled={false} customClass="delete_button" onClick={handleDelete} />
       </section>
     </div>
   );
@@ -35,6 +41,7 @@ TaskCard.propTypes = {
   title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   status: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
 };
 
 export default TaskCard;
