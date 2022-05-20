56def9cdae49deaa58203362626663535c8bbcc9
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 23:24:07 -0300

    feat: moves function to request tasks to provider

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -1,32 +1,20 @@
 /* eslint-disable no-unused-expressions */
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Components/Header';
 import TaskCard from '../Components/TaskCard';
-import { requestTasks } from '../Services/requests';
+import TasksContext from '../Context/TasksContext';
 import '../Styles/Pages/Task.css';
 
 function Tasks() {
   const navigate = useNavigate();
-  const [tasks, setTasks] = useState([]);
+  const { tasks } = useContext(TasksContext);
 
   // redirect to login page if not logged in
   useEffect(() => {
     const isLogged = JSON.parse(localStorage.getItem('logged'));
     if (!isLogged) navigate('/');
   });
-
-  useEffect(() => {
-    const { id } = JSON.parse(localStorage.getItem('user'));
-    const getTasks = async (userId) => {
-      const data = await requestTasks(userId);
-      data.tasks.push({ status: 'new' });
-      setTasks(data.tasks);
-    };
-
-    getTasks(id)
-      .catch(console.error);
-  }, []);
 
   return (
     <div>
@@ -38,6 +26,7 @@ function Tasks() {
               title={task.title}
               description={task.description}
               status={task.status}
+              id={task.id}
             />
           ))
         }
