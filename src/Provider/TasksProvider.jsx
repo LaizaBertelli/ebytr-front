36262085105730b214f4dac57efc75d2455dcd9e
LaizaBertelli <laizabertelli@hotmail.com>
2022-05-19 23:23:04 -0300

    feat: implements Tasks Provider

---
+++ src/Provider/TasksProvider.jsx
@@ -0,0 +1,36 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+import { React, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import TasksContext from '../Context/TasksContext';
+import { requestTasks } from '../Services/requests';
+
+function TasksProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const { id } = JSON.parse(localStorage.getItem('user'));
+      const data = await requestTasks(id);
+      data.tasks.push({ status: 'new' });
+      setTasks(data.tasks);
+    };
+    getTasks().catch(console.error);
+  }, []);
+
+  const context = {
+    tasks,
+    setTasks,
+  };
+
+  return (
+    <TasksContext.Provider value={context}>
+      { children }
+    </TasksContext.Provider>
+  );
+}
+
+TasksProvider.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.node).isRequired,
+};
+
+export default TasksProvider;
--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import TasksProvider from './Provider/TasksProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <TasksProvider>
+        <App />
+      </TasksProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
